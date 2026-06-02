f6c23da4f68cb67bbe7a90681ab3b1673132ab9f
actuelroi <yebouareel@gmail.com>
2026-06-02 23:48:48 +0200

    07: Carte set up and final ui version

--- components/carte-components/carte-drawer.tsx
+++ components/carte-components/carte-drawer.tsx
@@ -1,24 +1,17 @@
 
-
+'use client'
 import { Button } from "@/components/ui/button"
-import {
-    Drawer,
-    DrawerClose,
-    DrawerContent,
 
-    DrawerFooter,
-    DrawerHeader,
-    DrawerTitle,
-
-} from "@/components/ui/drawer"
 import { useCartDrawerStore } from "@/store/carte-drower.store"
 import { ChevronDown, X } from "lucide-react"
 import { Instrument_Sans } from "next/font/google";
+import { IoMdInformationCircle } from "react-icons/io";
 
 import Image from "next/image";
 
-import { Inria_Sans, Josefin_Sans, Indie_Flower } from "next/font/google";
+import { Inria_Sans, Josefin_Sans } from "next/font/google";
 import { Separator } from "../ui/separator";
+import { Sheet, SheetClose, SheetContent, SheetFooter, SheetHeader, SheetTitle } from "../ui/sheet"
 
 const inria = Inria_Sans({
     weight: '400'
@@ -28,9 +21,7 @@ const josefin = Josefin_Sans({
     weight: '400'
 });
 
-const Indie = Indie_Flower({
-    weight: '400'
-});
+
 
 
 
@@ -40,17 +31,22 @@ const instrumentSerif = Instrument_Sans({
 
 export function CarteDrawer() {
 
-    const { isOpen, onClose } = useCartDrawerStore()
+    const { isOpen, onClose, onOpen } = useCartDrawerStore()
 
     return (
-        <Drawer direction="right" open={isOpen} onOpenChange={onClose}>
-            <DrawerContent>
-                <DrawerHeader className="flex flex-row items-center justify-between">
-                    <DrawerTitle>Votre panier</DrawerTitle>
-                    <X onClick={onClose} className="cursor-pointer" />
-                </DrawerHeader>
-                <div className="flex flex-col gap-2 px-6 pt-6 ">
-                    <h1 className={`text-start ${instrumentSerif.className} text-lg`}>Sacs</h1>
+        <Sheet open={isOpen}
+            onOpenChange={(open) => {
+                if (open) onOpen()
+                else onClose()
+            }}>
+            <SheetContent className="">
+                <SheetHeader className="flex flex-row items-center justify-between">
+                    <SheetTitle>Votre panier (2 articles)</SheetTitle>
+
+                </SheetHeader>
+                <div className="flex flex-col overflow-y-auto">
+<div className="flex flex-col gap-2 px-6  ">
+                    <h1 className={`text-start ${instrumentSerif.className} text-sm`}>Sacs</h1>
                     <Separator />
                 </div>
                 <div className="flex gap-4 p-4">
@@ -94,17 +90,177 @@ export function CarteDrawer() {
                             <ChevronDown className="size-5" />
                         </div>
 
-                       <Button variant={'destructive'}>Supprimer</Button>
+                        <Button variant={'destructive'}>Supprimer</Button>
                     </div>
                 </div>
-                <h2 className="p-4 text-start text-sm">Completer vos choix</h2>
-                <DrawerFooter>
+                <h2 className="p-4 text-start text-sm underline">Completer vos choix</h2>
+
+
+                <div className="flex grid grid-cols-2 sm:grid-cols-3 lg:grid-cols gap-1">
+
+                    <div className="flex gap-1 p-1">
+                        <div className="w-15 h-20 relative">
+                            <Image src={'/Image/head.png'} alt="hero-1" fill />
+                        </div>
+
+                        <div
+                            className={`w-full flex flex-col  gap-1 ${inria.className}`}
+                        >
+                            <h1 className="  text-xs">
+                                Croquettes Diamond Naturals
+                            </h1>
+                            <div className="flex items-center justify-between  ">
+
+                                <p className={`text-xs  ${josefin.className} text-[#014338] font-light`}>
+                                    4.15 €
+                                </p>
+
+                            </div>
+                            <div className={`flex flex-col gap-1  `}>
+                                <p className={`text-xs  font-light mt-1 ${josefin.className} `}>
+                                    Flavor: Lamb Meal & Rice
+                                </p>
+
+                            </div>
+
+
+
+                            <div className={`flex flex-col gap-1 `}>
+                                <p className={`text-xs  font-light mt-1 ${josefin.className} `}>
+                                    Taille: 40kg
+                                </p>
+
+                            </div>
+
+                            <div className={`flex items-center  gap-1 mt-1 `}>
+                                <p className={`text-xs  font-light  ${josefin.className} `}>
+                                    Quantite:
+                                </p>
+                                <ChevronDown className="size-2" />
+                            </div>
+                            <Button size={'xs'}>
+                                Ajouter
+                            </Button>
+                        </div>
+                    </div>
+
+                    <div className="flex gap-1 p-1">
+                        <div className="w-15 h-20 relative">
+                            <Image src={'/Image/head.png'} alt="hero-1" fill />
+                        </div>
+
+                        <div
+                            className={`w-full flex flex-col  gap-1 ${inria.className}`}
+                        >
+                            <h1 className="  text-xs">
+                                Croquettes Diamond Naturals
+                            </h1>
+                            <div className="flex items-center justify-between  ">
+
+                                <p className={`text-xs  ${josefin.className} text-[#014338] font-light`}>
+                                    4.15 €
+                                </p>
+
+                            </div>
+                            <div className={`flex flex-col gap-1  `}>
+                                <p className={`text-xs  font-light mt-1 ${josefin.className} `}>
+                                    Flavor: Lamb Meal & Rice
+                                </p>
+
+                            </div>
+
+
+
+                            <div className={`flex flex-col gap-1 `}>
+                                <p className={`text-xs  font-light mt-1 ${josefin.className} `}>
+                                    Taille: 40kg
+                                </p>
+
+                            </div>
+
+                            <div className={`flex items-center  gap-1 mt-1 `}>
+                                <p className={`text-xs  font-light  ${josefin.className} `}>
+                                    Quantite:
+                                </p>
+                                <ChevronDown className="size-2" />
+                            </div>
+                            <Button size={'xs'}>
+                                Ajouter
+                            </Button>
+                        </div>
+                    </div>
+
+                    <div className="flex gap-1 p-1">
+                        <div className="w-15 h-20 relative">
+                            <Image src={'/Image/head.png'} alt="hero-1" fill />
+                        </div>
+
+                        <div
+                            className={`w-full flex flex-col  gap-1 ${inria.className}`}
+                        >
+                            <h1 className="  text-xs">
+                                Croquettes Diamond Naturals
+                            </h1>
+                            <div className="flex items-center justify-between  ">
+
+                                <p className={`text-xs  ${josefin.className} text-[#014338] font-light`}>
+                                    4.15 €
+                                </p>
+
+                            </div>
+                            <div className={`flex flex-col gap-1  `}>
+                                <p className={`text-xs  font-light mt-1 ${josefin.className} `}>
+                                    Flavor: Lamb Meal & Rice
+                                </p>
+
+                            </div>
+
+
+
+                            <div className={`flex flex-col gap-1 `}>
+                                <p className={`text-xs  font-light mt-1 ${josefin.className} `}>
+                                    Taille: 40kg
+                                </p>
+
+                            </div>
+
+                            <div className={`flex items-center  gap-1 mt-1 `}>
+                                <p className={`text-xs  font-light  ${josefin.className} `}>
+                                    Quantite:
+                                </p>
+                                <ChevronDown className="size-2" />
+                            </div>
+                            <Button size={'xs'}>
+                                Ajouter
+                            </Button>
+                        </div>
+                    </div>
+
+
+                </div>
+
+                <Separator />
+
+                <div className="flex items-center justify-between p-4">
+                    <h1>Sous-Total</h1>
+                    <h1 className="font-semibold text-lg">45 €</h1>
+                </div>
+                <div className="flex items-center gap-2 p-4">
+                    <p className="text-sm text-gray-400 ">Livraison prevue le 25 Juin
+                    </p>
+                    <IoMdInformationCircle className="size-4" />
+                </div>
+
+                </div>
+                
+
+                <SheetFooter>
                     <Button>Passer au paiement</Button>
-                    <DrawerClose asChild>
+                    <SheetClose asChild>
                         <Button variant="outline">Vider le panier</Button>
-                    </DrawerClose>
-                </DrawerFooter>
-            </DrawerContent>
-        </Drawer>
+                    </SheetClose>
+                </SheetFooter>
+            </SheetContent>
+        </Sheet>
     )
 }
